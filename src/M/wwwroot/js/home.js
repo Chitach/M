57c5591d0866b795f6dbe8f808d396120ddeb20f
Mike <Gd.misha@gmail.com>
2017-02-19 23:17:13 +0200

    The last posts widget added

---
+++ src/M/wwwroot/js/home.js
@@ -0,0 +1,29 @@
+﻿var scrollTime = 1000;
+
+$(document).ready(function () {
+	var hash = location.hash;
+	if (hash != "" && hash.length > 1) {
+		scrollToHash(hash);
+	}
+
+	$("a").on('click', function (event) {
+		var hash = this.hash;
+		scrollToHash(hash);
+		if (history.pushState) {
+			history.pushState(null, null, hash);
+		}
+		else {
+			location.hash = hash;
+		}
+
+		return false;
+	});
+
+	return false;
+});
+
+function scrollToHash(hash) {
+	$('html, body').animate({
+		scrollTop: $(hash).offset().top
+	}, scrollTime);
+}
