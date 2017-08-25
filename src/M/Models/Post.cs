40d791335b55388dc41f741a90d9afcc199967b1
Міша Гожда <gd.misha@gmail.com>
2017-08-25 23:35:19 +0300

    Short view added

--- src/M/Models/Post.cs
+++ src/M/Models/Post.cs
@@ -6,6 +6,9 @@ namespace M.Models {
 		#region private
 		#endregion
 
+		private const int ConstDefaultShortPostTextLength = 150;
+		private const int ConstMaxShortPostTextLength = 200;
+
 		#region Public properties
 		public int Id { get; set; }
 		public string Title { get; set; }
@@ -48,6 +51,26 @@ namespace M.Models {
 			}
 
 			return sb.ToString();
+		}
+
+		public string GetShortText() {
+			if (Text.Length < ConstDefaultShortPostTextLength) {
+				return Text;
+			}
+
+			int nextSpacePos = Text.IndexOf(" ", ConstDefaultShortPostTextLength);
+			char[] charsToTrim = { '.', ',', '!', '?', ':' };
+
+			if (nextSpacePos == -1) {
+				// break if too long word (> (ConstDefaultShortPostTextLength - ConstMaxShortPostTextLength))
+				if (Text.Length > ConstMaxShortPostTextLength) {
+					return Text.Substring(0, ConstMaxShortPostTextLength).Trim(charsToTrim) + "...";
+				}
+
+				return Text;
+			}
+
+			return Text.Substring(0, nextSpacePos).Trim(charsToTrim) + "...";
 		}
 
 	}
